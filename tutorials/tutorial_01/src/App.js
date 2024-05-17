5d05bf6c036154f9d56ce49852d9cedac3234321
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-05-17 17:24:01 +0200

    Tutorial 1 complete

---
+++ tutorials/tutorial_01/src/App.js
@@ -0,0 +1,10 @@
+import './css/App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./dynamic_table/DynamicTable";
+function App() {
+  return (
+    <DynamicTable />
+  );
+  
+}
+export default App;
---
+++ tutorials/tutorial_01/src/dynamic_table/TableData.js
@@ -0,0 +1,8 @@
+const TableData=[
+    {id:1, fullName:"Eben JvR", age:25, city:"Waterval Boven"},
+    {id:2, fullName:"Rourke", age:25, city:"Nelspruit"},
+    {id:3, fullName:"Dave", age:20, city:"Joburg"},
+    {id:4, fullName:"Link", age:20, city: "Pretoria"},
+    {id:5, fullName:"Kamva", age: 20, city: "???"}
+]
+export default TableData;
